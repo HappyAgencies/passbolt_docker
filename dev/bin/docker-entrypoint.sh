1d021f226db8ad12b515563804074b3bfdf97b08
Ariel <11020595+Draidel@users.noreply.github.com>
2025-03-15 12:31:30 -0300

    comment entropy_check()

--- dev/bin/docker-entrypoint.sh
+++ dev/bin/docker-entrypoint.sh
@@ -12,28 +12,28 @@ subscription_key_file_paths=("/etc/passbolt/subscription_key.txt" "/etc/passbolt
 
 export GNUPGHOME="/home/www-data/.gnupg"
 
-entropy_check() {
-  local entropy_avail
-
-  entropy_avail=$(cat /proc/sys/kernel/random/entropy_avail)
-
-  if [ "$entropy_avail" -lt 2000 ]; then
-
-    cat <<EOF
-==================================================================================
-  Your entropy pool is low. This situation could lead GnuPG to not
-  be able to create the gpg serverkey so the container start process will hang
-  until enough entropy is obtained.
-  Please consider installing rng-tools and/or virtio-rng on your host as the
-  preferred method to generate random numbers using a TRNG.
-  If rngd (rng-tools) does not provide enough or fast enough randomness you could
-  consider installing haveged as a helper to speed up this process.
-  Using haveged as a replacement for rngd is not recommended. You can read more
-  about this topic here: https://lwn.net/Articles/525459/
-==================================================================================
-EOF
-  fi
-}
+#entropy_check() {
+#  local entropy_avail
+#
+#  entropy_avail=$(cat /proc/sys/kernel/random/entropy_avail)
+#
+# if [ "$entropy_avail" -lt 2000 ]; then
+#
+#    cat <<EOF
+#==================================================================================
+#  Your entropy pool is low. This situation could lead GnuPG to not
+#  be able to create the gpg serverkey so the container start process will hang
+#  until enough entropy is obtained.
+#  Please consider installing rng-tools and/or virtio-rng on your host as the
+#  preferred method to generate random numbers using a TRNG.
+#  If rngd (rng-tools) does not provide enough or fast enough randomness you could
+#  consider installing haveged as a helper to speed up this process.
+#  Using haveged as a replacement for rngd is not recommended. You can read more
+#  about this topic here: https://lwn.net/Articles/525459/
+#==================================================================================
+#EOF
+#  fi
+#}
 
 gpg_gen_key() {
   key_email="${PASSBOLT_KEY_EMAIL:-passbolt@yourdomain.com}"
